d713cd6a773bdb765a4840c9a8f206a15ca68c86
camiodvl <camilo.228@hotmail.com>
2022-12-10 17:39:18 -0500

    Readme1

--- src/components/programs/ProgramDetail.vue
+++ src/components/programs/ProgramDetail.vue
@@ -53,13 +53,41 @@
                     <div class="card-body">
                         <h5 class="card-title">{{ curso.name }}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">Semestre {{ curso.pivot.semester }}</h6>
-                        <a href="#" class="card-link">Detalles</a>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" v-bind:data-target="'#'+curso.name">
+                            Detalles
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" v-bind:id="curso.name" tabindex="-1" role="dialog"
+                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">{{curso.name}}</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p><strong>Creditos:</strong> {{curso.credits}}</p>
+                                        <p><strong>Pre-Requisito:</strong> {{curso.pre_requisite}}</p>
+                                        <p><strong>Profesor:</strong> {{curso.teacher}}</p>
+                                        <p><strong>Horas autonomas:</strong> {{curso.a_hours}}</p>
+                                        <p><strong>Horas dirigidas:</strong> {{curso.d_hours}}</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary"
+                                            data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </template>
         </template>
     </div>
-
 
     <div class="container d-flex justify-content-center">
         <template v-for="curso in cursos">
@@ -68,14 +96,41 @@
                     <div class="card-body">
                         <h5 class="card-title">{{ curso.name }}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">Semestre {{ curso.pivot.semester }}</h6>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" v-bind:data-target="'#'+curso.name">
+                            Detalles
+                        </button>
 
+                        <!-- Modal -->
+                        <div class="modal fade" v-bind:id="curso.name" tabindex="-1" role="dialog"
+                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">{{curso.name}}</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p><strong>Creditos:</strong> {{curso.credits}}</p>
+                                        <p><strong>Pre-Requisito:</strong> {{curso.pre_requisite}}</p>
+                                        <p><strong>Profesor:</strong> {{curso.teacher}}</p>
+                                        <p><strong>Horas autonomas:</strong> {{curso.a_hours}}</p>
+                                        <p><strong>Horas dirigidas:</strong> {{curso.d_hours}}</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary"
+                                            data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
             </template>
         </template>
     </div>
-
 
     <div class="container d-flex justify-content-center">
         <template v-for="curso in cursos">
@@ -84,13 +139,41 @@
                     <div class="card-body">
                         <h5 class="card-title">{{ curso.name }}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">Semestre {{ curso.pivot.semester }}</h6>
-                        <a href="#" class="card-link">Detalles</a>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" v-bind:data-target="'#'+curso.name">
+                            Detalles
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" v-bind:id="curso.name" tabindex="-1" role="dialog"
+                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">{{curso.name}}</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p><strong>Creditos:</strong> {{curso.credits}}</p>
+                                        <p><strong>Pre-Requisito:</strong> {{curso.pre_requisite}}</p>
+                                        <p><strong>Profesor:</strong> {{curso.teacher}}</p>
+                                        <p><strong>Horas autonomas:</strong> {{curso.a_hours}}</p>
+                                        <p><strong>Horas dirigidas:</strong> {{curso.d_hours}}</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary"
+                                            data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </template>
         </template>
     </div>
-
 
     <div class="container d-flex justify-content-center">
         <template v-for="curso in cursos">
@@ -99,13 +182,41 @@
                     <div class="card-body">
                         <h5 class="card-title">{{ curso.name }}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">Semestre {{ curso.pivot.semester }}</h6>
-                        <a href="#" class="card-link">Detalles</a>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" v-bind:data-target="'#'+curso.name">
+                            Detalles
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" v-bind:id="curso.name" tabindex="-1" role="dialog"
+                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">{{curso.name}}</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p><strong>Creditos:</strong> {{curso.credits}}</p>
+                                        <p><strong>Pre-Requisito:</strong> {{curso.pre_requisite}}</p>
+                                        <p><strong>Profesor:</strong> {{curso.teacher}}</p>
+                                        <p><strong>Horas autonomas:</strong> {{curso.a_hours}}</p>
+                                        <p><strong>Horas dirigidas:</strong> {{curso.d_hours}}</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary"
+                                            data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </template>
         </template>
     </div>
-
 
     <div class="container d-flex justify-content-center">
         <template v-for="curso in cursos">
@@ -114,73 +225,41 @@
                     <div class="card-body">
                         <h5 class="card-title">{{ curso.name }}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">Semestre {{ curso.pivot.semester }}</h6>
-                        <a href="#" class="card-link">Detalles</a>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" v-bind:data-target="'#'+curso.name">
+                            Detalles
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" v-bind:id="curso.name" tabindex="-1" role="dialog"
+                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">{{curso.name}}</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p><strong>Creditos:</strong> {{curso.credits}}</p>
+                                        <p><strong>Pre-Requisito:</strong> {{curso.pre_requisite}}</p>
+                                        <p><strong>Profesor:</strong> {{curso.teacher}}</p>
+                                        <p><strong>Horas autonomas:</strong> {{curso.a_hours}}</p>
+                                        <p><strong>Horas dirigidas:</strong> {{curso.d_hours}}</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary"
+                                            data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </template>
         </template>
     </div>
-
-
-    <div class="container d-flex justify-content-center">
-        <template v-for="curso in cursos">
-            <template v-if="curso.pivot.semester == 7">
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ curso.name }}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Semestre {{ curso.pivot.semester }}</h6>
-                        <a href="#" class="card-link">Detalles</a>
-                    </div>
-                </div>
-            </template>
-        </template>
-    </div>
-
-
-    <div class="container d-flex justify-content-center">
-        <template v-for="curso in cursos">
-            <template v-if="curso.pivot.semester == 8">
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ curso.name }}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Semestre {{ curso.pivot.semester }}</h6>
-                        <a href="#" class="card-link">Detalles</a>
-                    </div>
-                </div>
-            </template>
-        </template>
-    </div>
-
-
-    <div class="container d-flex justify-content-center">
-        <template v-for="curso in cursos">
-            <template v-if="curso.pivot.semester == 9">
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ curso.name }}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Semestre {{ curso.pivot.semester }}</h6>
-                        <a href="#" class="card-link">Detalles</a>
-                    </div>
-                </div>
-            </template>
-        </template>
-    </div>
-
-
-    <div class="container d-flex justify-content-center">
-        <template v-for="curso in cursos">
-            <template v-if="curso.pivot.semester == 10">
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ curso.name }}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Semestre {{ curso.pivot.semester }}</h6>
-                        <a href="#" class="card-link">Detalles</a>
-                    </div>
-                </div>
-            </template>
-        </template>
-    </div>
-
 
 
 
